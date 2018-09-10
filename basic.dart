e60e2db20580295706569886b55288f473aab076
honest <1131328108@qq.com>
2018-09-10 10:00:39 +0800

    入口，定义变量

---
+++ basic.dart
@@ -0,0 +1,21 @@
+// 这个是 dart 的入口
+// 函数前面表示是否返回，和Java很像
+void main() {
+  print('你好');
+
+  var a;
+  a = 1;
+  a = "1";
+  a = true;
+  a = [1];
+  a = { 1: 1 };
+
+  int b;
+  b = 2;
+  // b = "3"; 不能这样使用
+
+  final String email = "honestxll@gmail.com";
+
+  num c = 2;
+  double d = 3.3;
+}
